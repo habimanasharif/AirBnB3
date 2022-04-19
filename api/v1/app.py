a39782a2c8a24a1e6388bfe91d6758673109ed63
sharifhabiman <hdidiersharif@gmail.com>
2022-04-19 15:22:26 +0200

    task 2

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,37 @@
-from importlib import resources
-from logging import exception
-import resource
-from flask import Flask, make_response, jsonify
+#!/usr/bin/python3
+"""
+    Task 4 ASDFKJA;LDFKA;LDFK;AF
+"""
 from models import storage
-import os
 from api.v1.views import app_views
+from flask import Blueprint, Flask, jsonify, make_response
 from flask_cors import CORS
+import os
 
-app= Flask(__name__)
-app.regester_blueprint(app_views)
-CORS(app,resources={r"*/":{"origins":"0.0.0.0"}})
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, origins="0.0.0.0")
+
 
 @app.teardown_appcontext
-def close(exception):
-    """ this function will close a session and reload"""
-    storage.close
+def tearthatmotherfuckerdown(self):
+    """ tear that shit down """
+    storage.close()
+
 
 @app.errorhandler(404)
-def error(error):
-    """ this fuction will jsonfy when there is an error"""
-    return make_response(jsonify({"error":"Not found"}),404)
+def dudewermypge(e):
+    """ dude, where's my page """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
-if __name__ == "__main__":
-    app.run(host = os.getenv('HBNB_API_HOST'), port = os.getenv('HBNB_API_PORT'),
-            threaded = True)
+
+if __name__ == '__main__':
+    ho = os.getenv("HBNB_API_HOST")
+    po = os.getenv("HBNB_API_PORT")
+    if ho is None:
+        ho = '0.0.0.0'
+    if po is None:
+        po = 5000
+
+    app.run(host=ho, port=po, threaded=True, debug=True)
